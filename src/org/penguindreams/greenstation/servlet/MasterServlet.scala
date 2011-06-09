d7f0062c816cb1484d53a77d6c25e03f17a31204
U-AD\khannast <khannast@KhannastM4500.ad.uc.edu>
2011-06-09 16:53:00 -0400

    path URL function

--- src/org/penguindreams/greenstation/servlet/MasterServlet.scala
+++ src/org/penguindreams/greenstation/servlet/MasterServlet.scala
@@ -6,5 +6,8 @@ import javax.servlet.http.{HttpServlet,
 class MasterServlet extends HttpServlet {
 
   override def service(req : HSReq, resp: HSResp) =
-    resp.getWriter().write("<html><body>Hello World</body></html>")
+    resp.getWriter().write("<html><body>%s</body></html>".format(getPath(req)))
+    
+  def getPath(req: HSReq) =
+    req.getRequestURI().substring( req.getContextPath().length() + req.getServletPath().length() );
 }
