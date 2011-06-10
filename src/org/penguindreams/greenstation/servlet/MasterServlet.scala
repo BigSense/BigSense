d113b4352dd97177d9e363839d97eecbc536b06f
Sumit Khanna <sum.notify@gmail.com>
2011-06-10 00:58:33 -0400

    Started working on JSP views with JSTL; got into an infinite loop

--- src/org/penguindreams/greenstation/servlet/MasterServlet.scala
+++ src/org/penguindreams/greenstation/servlet/MasterServlet.scala
@@ -5,9 +5,13 @@ import javax.servlet.http.{HttpServlet,
 
 class MasterServlet extends HttpServlet {
 
-  override def service(req : HSReq, resp: HSResp) =
-    resp.getWriter().write("<html><body>%s</body></html>".format(getPath(req)))
+  override def service(req : HSReq, resp: HSResp) = {
+    var ops = getPath(req);
+    req.setAttribute("hi","someValue");
+    req.getRequestDispatcher("./test.jsp").forward(req,resp)
+    //resp.getWriter().write("<html><body>%s</body></html>".format(ops(0)))
+  }
     
   def getPath(req: HSReq) =
-    req.getRequestURI().substring( req.getContextPath().length() + req.getServletPath().length() );
+    req.getRequestURI().substring( req.getContextPath().length() + req.getServletPath().length() ).split("/");
 }
---
+++ web/test.jsp
@@ -0,0 +1,10 @@
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<html>
+<body>
+
+<c:out value="${hi}" />
+
+<p>Test</p>
+
+</body>
+</html>
