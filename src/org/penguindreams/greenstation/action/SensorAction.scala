450d23e58c137a01fb2555482b29e66f60e720df
Sumit Khanna <sum.notify@gmail.com>
2011-06-14 09:30:50 -0400

    minor changes.

--- src/org/penguindreams/greenstation/action/SensorAction.scala
+++ src/org/penguindreams/greenstation/action/SensorAction.scala
@@ -1,7 +1,9 @@
 package org.penguindreams.greenstation.action {
 
+  import java.util.Map;
+  
 	class SensorAction extends ActionTrait {	
-	  def runAction(method: String, args: Array[String], postData: String): ActionResponse = { null }
+	  def runAction(method: String, args: Array[String], parameters: Map): ActionResponse = { null }
 	}
 
 }
