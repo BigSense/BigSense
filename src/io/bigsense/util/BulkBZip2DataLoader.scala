6c2434425c316497f6a9eacc3f326351ecec15a3
Sumit Khanna <sumit@catalyst.net.nz>
2013-11-21 17:27:48 +1300

    more work on the loader (currently broken)

--- src/io/bigsense/util/BulkBZip2DataLoader.scala
+++ src/io/bigsense/util/BulkBZip2DataLoader.scala
@@ -1,9 +1,54 @@
 
+package io.bigsense.util
+
+import java.io.{ByteArrayOutputStream, BufferedInputStream, FileInputStream, File}
+import org.apache.commons.compress.archivers.tar.{TarArchiveEntry, TarArchiveInputStream}
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
+import io.bigsense.spring.MySpring
+import org.apache.commons.compress.utils.IOUtils
+import io.bigsense.format.{FormatTrait, AgraDataXMLFormat}
+import scala.collection.mutable.ListBuffer
+import io.bigsense.model.ModelTrait
+import io.bigsense.db.ServiceDataHandlerTrait
+
+
 object BulkBZip2DataLoader {
 
 
+  val BUFFER_SIZE = 4096
+  val PACKAGE_CHUNK_SIZE = 10000
+
   def main(args : Array[String])  {
 
+    if(args.length != 1) {
+      System.err.println("Usage: %s [bzip2 file]".format(this.getClass))
+      System.exit(1)
+    }
+
+    val loader = MySpring.getObject("FormatAGRA.XML").asInstanceOf[FormatTrait]
+    val db     = MySpring.getObject("serviceDataHandler").asInstanceOf[ServiceDataHandlerTrait]
+
+    val bzin = new TarArchiveInputStream(new BZip2CompressorInputStream(new BufferedInputStream(new FileInputStream(args(0)))))
+    var entry : TarArchiveEntry = null;
+    val models = new ListBuffer[ModelTrait]()
+    var chunks = 0
+
+    while( (entry = bzin.getNextEntry().asInstanceOf[TarArchiveEntry]) != null  ) {
+      if(entry.isFile()) {
+
+        val xmlfile = new ByteArrayOutputStream()
+        IOUtils.copy(bzin,xmlfile)
+        models.append( loader.loadModels(new String(xmlfile.toByteArray())) )
+
+        chunks = chunks + 1
+        if( chunks % PACKAGE_CHUNK_SIZE) {
+          System.out.println("Sending batch of %d to database".format(PACKAGE_CHUNK_SIZE))
+          db.loadData(models.toList)
+          models.clear()
+        }
+      }
+
+    }
 
 
   }
