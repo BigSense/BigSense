3a1ec313ffae6dd9b38c509f39e157b0dc26fd90
Sumit Khanna <sumit@catalyst.net.nz>
2014-01-14 13:24:37 +1300

    MySQL aggrigation works (incorrect times)

--- ddl/mysql/000-DDLInitilization.sql
+++ ddl/mysql/000-DDLInitilization.sql
@@ -1,6 +1,20 @@
 CREATE DATABASE BigSense;
 GRANT ALL ON BigSense.* TO 'db_bigsense_ddl'@'localhost' IDENTIFIED BY 'insertpassword';
-GRANT SELECT,INSERT,UPDATE,DELETE ON BigSense.* TO 'db_bigsense'@'localhost' IDENTIFIED BY 'insertpassword';
+GRANT SELECT,INSERT,UPDATE,DELETE,EXECUTE ON BigSense.* TO 'db_bigsense'@'localhost' IDENTIFIED BY 'insertpassword';
 
 USE BigSense;
 CREATE TABLE ddl_info( id INT auto_increment PRIMARY KEY, version INT, installed TIMESTAMP );
+
+delimiter $$
+drop function if exists `isnumeric` $$
+create function `isnumeric` (s varchar(255)) returns int deterministic
+begin
+set @match =
+   '^(([0-9+-.$]{1})|([+-]?[$]?[0-9]*(([.]{1}[0-9]*)|([.]?[0-9]+))))$';
+
+return if(s regexp @match, 1, 0);
+end $$
+
+delimiter ;
+
+GRANT EXECUTE ON PROCEDURE BigSense.isnumeric TO 'db_bigsense'@'localhost'
