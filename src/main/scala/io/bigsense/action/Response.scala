fbd5144cfce51c0396fee886c3ceee9f62367b46
Sumit Khanna <sumit@catalyst.net.nz>
2014-04-15 16:15:09 +1200

    Cleaup

--- src/main/scala/io/bigsense/action/Response.scala
+++ src/main/scala/io/bigsense/action/Response.scala
@@ -1,20 +1,31 @@
 package io.bigsense.action
 
+import javax.servlet.http.HttpServletResponse
+
 /**
  * Created by sumit on 4/11/14.
  */
 
 class Response (
-  val status : Int,
-  val newLocations : List[Int],
-  val contentType : Option[String] = None
+  val status : Int = HttpServletResponse.SC_OK,
+  val contentType : Option[String] = None,
+  val newLocations : List[Int] = List()
 )
 
-class BinaryResponse(val output : Array[Byte], status : Int, newLocations: List[Int], contentType : Option[String])
-  extends Response(status,newLocations,contentType)
+class BinaryResponse(val output : Array[Byte],
+                     status : Int = HttpServletResponse.SC_OK,
+                     contentType : Option[String] = Some("application/octet-stream"),
+                     newLocations: List[Int] = List())
+  extends Response(status,contentType,newLocations)
 
-class StringResponse(status : Int, newLocations: List[Int], contentType : Option[String], val output : String)
-  extends Response(status,newLocations,contentType)
+class StringResponse(val output : String,
+                     status : Int = HttpServletResponse.SC_OK,
+                     contentType : Option[String] = Some("text/plain"),
+                     newLocations: List[Int] = List())
+  extends Response(status,contentType,newLocations)
 
-class ViewResponse(status : Int, newLocations: List[Int], contentType : Option[String], val view : String, val viewData : Map[String,Any])
-  extends Response(status,newLocations,contentType)
+class ViewResponse(val view : String, val viewData : Map[String,Any],
+                   status : Int = HttpServletResponse.SC_OK,
+                   contentType : Option[String] = Some("text/html"),
+                   newLocations: List[Int] = List())
+  extends Response(status,contentType,newLocations)
