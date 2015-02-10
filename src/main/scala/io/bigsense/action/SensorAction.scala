3d8ee7034789a7a93f63dbb0266de7b8a47429c5
Sumit Khanna <sumit@catalyst.net.nz>
2015-02-10 16:31:02 +1300

    Removed old SmartVision and N*U rubbish

--- src/main/scala/io/bigsense/action/SensorAction.scala
+++ src/main/scala/io/bigsense/action/SensorAction.scala
@@ -8,10 +8,6 @@
 package io.bigsense.action {
 
 import javax.servlet.http.HttpServletResponse
-import io.bigsense.spring.MySpring
-import io.bigsense.processor.ProcessorTrait
-import io.bigsense.model.SensorModel
-import io.bigsense.processor.ProcessorException
 
 class SensorAction extends ActionTrait {
 
@@ -19,32 +15,6 @@ class SensorAction extends ActionTrait {
 
     aReq.method match {
       case "POST" => {
-
-        /*
-         * This entire section is for N*Us or "Non-Processed <Type> Units"
-         */
-        aReq.models.foreach(packs => {
-          packs.sensors.foreach(sens => {
-            val nPat = "N(.*)U".r //Regex for NImageU, NCounterU, etc.
-
-            sens.units match {
-              case nPat(processor) => {
-                try {
-                  packs.processed.append(MySpring.getObject(processor + "Processor").asInstanceOf[ProcessorTrait].processModels(sens))
-                }
-                catch {
-                  case e: ProcessorException => {
-                    packs.processed.append(sens.clone().asInstanceOf[SensorModel])
-                    sens.data = ""
-                    packs.errors.append(e.getMessage())
-                  }
-                }
-              }
-              case _ => {}
-            }
-          })
-        })
-
         new StringResponse("", HttpServletResponse.SC_CREATED, newLocations = this.dbHandler.loadData(aReq.models))
       }
       case "GET" => {
--- src/main/scala/io/bigsense/processor/ProcessorException.scala
+++
@@ -1,4 +0,0 @@
-package io.bigsense.processor
-
-
-case class ProcessorException(message: String) extends RuntimeException(message) {}
