13e4af4b799d9d2e2bf6abed425c02299e51b889
Sumit Khanna <sumit@penguindreams.org>
2015-08-13 20:14:47 +0800

    removed obsolete spring relic

--- src/main/scala/io/bigsense/security/SecurityManagerTrait.scala
+++ src/main/scala/io/bigsense/security/SecurityManagerTrait.scala
@@ -1,10 +1,9 @@
 package io.bigsense.security
 import io.bigsense.action.ActionRequest
-import scala.reflect.BeanProperty
 import io.bigsense.db.ServiceDataHandlerTrait
 
 trait SecurityManagerTrait {
 
   def securityFilter(request : ActionRequest) : Boolean
-  @BeanProperty var dbHandler : ServiceDataHandlerTrait = _
+  var dbHandler : ServiceDataHandlerTrait = _
 }
