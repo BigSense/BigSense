3bc53bf485c73d6182f6970f55770ddc6565fab5
Sumit Khanna <sumit@penguindreams.org>
2016-04-06 23:32:09 -0700

    Initial Json stubs

---
+++ src/main/scala/io/bigsense/format/SenseJsonFormat.scala
@@ -0,0 +1,14 @@
+package io.bigsense.format
+
+import io.bigsense.model.ModelTrait
+
+/**
+  * Created by cassius on 6/04/16.
+  */
+class SenseJsonFormat extends FormatTrait {
+
+  override def renderModels(model: List[ModelTrait]): String = ""
+  override def loadModels(data: String): List[ModelTrait] = null
+
+  override def mimeType = "application/bigsense+json"
+}
